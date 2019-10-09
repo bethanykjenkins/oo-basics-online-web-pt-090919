cd5fa460ea37c9e8e3303d629d1f131dbd1b65e5
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-09 01:20:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,45 +1,14 @@
 # Make your shoe class here!
 
 class Shoe 
+  
+  attr_accessor :brand, :color, :size, :material, condition
+  attr_reder :brand
+  
   def initialize(brand)
     @brand = brand
   end
   
-  def brand
-    @brand
-  end  
-  
-  def initialize(color)
-    @color = color
-  end
-  
-  def color
-    @color
-  end
-  
-  def initialize(size)
-    @size=size
-  end
-  
-  def size
-    @size
-  end
-  
-  def initialize(material)
-    @material=material
-  end
-  
-  def material
-    @material
-  end
-  
-  def initialize(condition)
-    @condition=condition
-  end
-  
-  def condition
-    @condition
-  end
     
   
 end
