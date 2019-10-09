bf278fd88668394b3c79bd086f4ba321570b91c4
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-09 01:17:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,45 @@
 # Make your shoe class here!
+
+def Shoe 
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end  
+  
+  def initialize(color)
+    @color = color
+  end
+  
+  def color
+    @color
+  end
+  
+  def initialize(size)
+    @size=size
+  end
+  
+  def size
+    @size
+  end
+  
+  def initialize(material)
+    @material=material
+  end
+  
+  def material
+    @material
+  end
+  
+  def initialize(condition)
+    @condition=condition
+  end
+  
+  def condition
+    @condition
+  end
+    
+  
+end
